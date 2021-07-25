15026d97b06e4756bfd4c53c54e93fc07c14ce36
Jeffrey Skinner <jeff@pyusoft.com>
2021-07-25 19:41:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
+# Ah shit, here we go again...
+
 echo "The Archy installation has started.";
 
+# Dewit
 sudo pacman -R \
-noto-fonts-cjk \
-noto-fonts-emoji \
-alacritty \
-ntfs-3g \
-gvfs \
-thunar \
-lxdm \
-rofi \
-alsa-utils \
-nvidia \
-chromium \
-i3status \
-i3 &&
+noto-fonts-cjk \   # Fonts
+noto-fonts-emoji \ # ^
+alacritty \        # Terminal
+ntfs-3g \          # File system
+gvfs \             # ^
+thunar \           # ^
+lxdm \             # Display manager
+rofi \             # Launcher
+alsa-utils \       # Audio
+nvidia \           # Graphics
+chromium \         # Web browser
+i3status \         # Window manager
+i3 &&              # ^
 
+# Enable lxdm service
 systemctl disable lxdm;
 
-echo "The Archy installtion has finished."
+echo "The Archy installtion has finished.";
+
+echo "Please reboot for changes.";
+
+# echo "Rebooting...";
+
+# reboot;
+
+# Sicko mode
