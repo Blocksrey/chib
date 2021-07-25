fa05f0308ff09bbde6ce2a704b6bcbdd0a5bb854
Jeffrey Skinner <jeff@pyusoft.com>
2021-07-25 19:14:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
-echo chinchin
+#!/bin/sh
+
+echo "The Archy installation has started." &&
+
+sudo pacman -S \
+gvfs \
+ntfs-3g \
+thunar \
+alacritty \
+lxdm \
+noto-fonts-cjk \
+noto-fonts-emoji \
+chromium \
+rofi \
+alsa-utils \
+i3 \
+i3 status \
+nvidia &&
+
+systemctl enable lxdm &&
+
+echo "The Archy installtion has finished."
