366be9211664d31e422a2f3c7efa248354c898a9
Jeffrey Skinner <jeff@pyusoft.com>
2021-07-25 22:58:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+#Ah shit, here we go again...
 echo "The Archy installation has started." &&
 
-sudo pacman -S \
-noto-fonts-cjk \
-noto-fonts-emoji \
-alacritty \
-ntfs-3g \
-gvfs \
-thunar \
-lxdm \
-rofi \
-alsa-utils \
-nvidia \
-firefox \
-i3status \
+#Dewit
+sudo pacman -S\ #
+p7zip\
+noto-fonts-cjk\
+noto-fonts-emoji\
+alacritty\
+ntfs-3g\
+gvfs\
+nnn\
+lxdm\
+rofi\
+alsa-utils\
+nvidia\
+firefox\
+i3status\
 i3 &&
+
 
 systemctl enable lxdm &&
 
