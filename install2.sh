a4cb4bd5c75a40ff112aa6f38e6aa38728d4c5e7
Jeffrey Skinner <jeff@pyusoft.com>
2021-07-26 00:02:46 +0000

    Casdasdasdkajshdaldkasld Aasdsad D A B B A A

---
+++ install2.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+##################################################
+# Archy, by Jeffrey Skinner.                     #
+# Archy is personalized specifically for myself, #
+# however feel free to use it as you'd like.     #
+##################################################
+
+# Package list
+packages=(
+	# Development
+	git
+	cmake
+
+	# File management
+	p7zip
+	nnn
+	gvfs
+	ntfs-3g
+
+	# Terminal emulation
+	alacritty
+
+	# Fonts
+	noto-fonts-cjk
+	noto-fonts-emoji
+
+	# Display manager
+	lxdm
+
+	# Application launcher
+	rofi
+
+	# Drivers
+	alsa-utils
+	nvidia
+
+	# Web browsing
+	firefox
+
+	# Window manager
+	i3status
+	i3
+
+	# Video player
+	mpv
+)
+
+# Ah shit, here we go again...
+
+installed=false
+pacman -S ${packages[@]} && installed=true
+
+if $installed
+then
+	echo "Packages have been successfully installed, configuration will begin."
+
+	systemctl enable lxdm &&
+	echo "Archy installation has succeeded, a reboot will be initiated." &&
+	reboot
+else
+	echo "Archy installation has failed, glhf."
+fi
