1c48087d886f43dee4878b4c632378013abfdf26
blocksrey <jeff@miner.attlocal.net>
2021-10-31 20:13:51 -0700

    Fixed permissions I think

--- nvidia-nvfbc/install.sh
+++ nvidia-nvfbc/install.sh
@@ -1,4 +1,8 @@
+cd $(dirname "$0") &&
 
 xbps-install -yS git &&
+git clone https://github.com/keylase/nvidia-patch.git &&
 
-git clone https://github.com/keylase/nvidia-patch.git
+./nvidia-patch/patch-fbc.sh &&
+
+rm -r nvidia-patch
