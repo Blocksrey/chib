d18453a1d6cdb2d4b0df92181a3f2e18bdfe4ce1
Jeffrey Skinner <Jeff@Pyusoft.com>
2022-07-18 11:14:47 -0700

    huh

--- group/nvlax/build/CPM_modules/FindPPK_ASSERT.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/jeff/ChibiLinux/group/nvlax/build/cmake/CPM_0.34.0.cmake")
-CPMAddPackage(NAME;PPK_ASSERT;GIT_TAG;master;GITHUB_REPOSITORY;gpakosz/PPK_ASSERT;DOWNLOAD_ONLY;YES)
-set(PPK_ASSERT_FOUND TRUE)
